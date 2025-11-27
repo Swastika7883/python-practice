89b2e0e14a08f2c26bfc864c57ccbbe8ba15917a
Swastika7883 <swastikabera2004@gmail.com>
2025-11-28 03:15:51 +0530

    edited

--- p4.py
+++ p4.py
@@ -6,3 +6,13 @@ def PrintTriangle(n):
 
 
 PrintTriangle(6)
+
+
+def PrintTriangle(n):
+    for i in range (n):
+        for j in range (i+1):
+            print("*", end=" ")
+        print()
+
+
+PrintTriangle(6)
